c1af49c2072216eb0e672ee3eb79215e1bc1cf0d
SunRise <1272165766@qq.com>
2022-03-12 15:10:05 +0800

    first commit

---
+++ src/main/java/com/example/Config/MySecurityConfig.java
@@ -0,0 +1,24 @@
+package com.example.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@EnableWebSecurity
+@Configuration
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/index").permitAll()
+                .antMatchers("/log").hasRole("vip1");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)  {
+    }
+}
---
+++ src/main/java/com/example/Controller/TestController.java
@@ -0,0 +1,16 @@
+package com.example.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+
+    @RequestMapping("/gotolog")
+    public String fun(){
+
+        return "log";
+    }
+
+}
